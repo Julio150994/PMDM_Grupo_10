fcc6e01c8ef193c1002757ff1e80b9fb165ecb67
AlbertoDiazHeras <93120404+AlbertoDiazHeras@users.noreply.github.com>
2021-12-06 20:34:47 +0100

    ion card componente

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListaPedidosComponent } from './lista-pedidos/lista-pedidos.component';
 import { ContenidoPedidoComponent } from './contenido-pedido/contenido-pedido.component';
-
+import { IonCardComponent} from './ion-card/ion-card.component';
 
 
 @NgModule({
   declarations: [
     ListaPedidosComponent,
-    ContenidoPedidoComponent
+    ContenidoPedidoComponent,
+    IonCardComponent,
   ],
   imports: [
     CommonModule,
+    IonCardComponent
   ],
   exports: [
     ListaPedidosComponent,
-    ContenidoPedidoComponent
+    ContenidoPedidoComponent,
+    IonCardComponent,
   ]
 })
 export class ComponentsModule { }
