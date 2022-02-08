b32e56aa6ccab7d91dc6905241b5d5486a7bbdc6
AlbertoDiazHeras <93120404+AlbertoDiazHeras@users.noreply.github.com>
2022-02-08 14:57:41 +0100

    comparación de fechas funcionanado

--- src/app/graficas/graficas.page.ts
+++ src/app/graficas/graficas.page.ts
@@ -178,14 +178,9 @@ export class GraficasPage implements OnInit {
 
   calcularCantidades(event){
 
-    moment().format("YYYY-MM-DD");
     let date = new Date();
 
-    var formatoFecha = new Date(date).toLocaleDateString('en-GB', {
-        month: 'numeric',
-        day: 'numeric',
-        year: 'numeric'
-    }).split('/').join('-');
+    var formatoFecha = new Date(date);
     
     console.log(formatoFecha);
 
@@ -195,11 +190,7 @@ export class GraficasPage implements OnInit {
     inicioMes5.setDate(1);
     inicioMes5.setMonth(date.getMonth() - 6);
 
-    var formatoFecha1 = new Date(inicioMes5).toLocaleDateString('en-GB', {
-      month: 'numeric',
-      day: 'numeric',
-      year: 'numeric'
-    }).split('/').join('-');
+    var formatoFecha1 = new Date(inicioMes5);
   
     console.log(formatoFecha1);
     let fecha1: moment.Moment = moment(formatoFecha1);
@@ -209,11 +200,7 @@ export class GraficasPage implements OnInit {
     finalMes5.setMonth(date.getMonth() - 6);
     finalMes5.setDate(31);
 
-    var formatoFecha2 = new Date(finalMes5).toLocaleDateString('en-GB', {
-      month: 'numeric',
-      day: 'numeric',
-      year: 'numeric'
-    }).split('/').join('-');
+    var formatoFecha2 = new Date(finalMes5);
   
     console.log(formatoFecha2);
     let fecha2: moment.Moment = moment(formatoFecha2);
@@ -221,11 +208,7 @@ export class GraficasPage implements OnInit {
     inicioMes4.setDate(1);
     inicioMes4.setMonth(date.getMonth() - 5);
 
-    var formatoFecha3 = new Date(inicioMes4).toLocaleDateString('en-GB', {
-      month: 'numeric',
-      day: 'numeric',
-      year: 'numeric'
-    }).split('/').join('-');
+    var formatoFecha3 = new Date(inicioMes4);
   
     console.log(formatoFecha3);
     let fecha3: moment.Moment = moment(formatoFecha3);
@@ -235,11 +218,7 @@ export class GraficasPage implements OnInit {
     finalMes4.setMonth(date.getMonth() - 5);
     finalMes4.setDate(30);
 
-    var formatoFecha4 = new Date(finalMes4).toLocaleDateString('en-GB', {
-      month: 'numeric',
-      day: 'numeric',
-      year: 'numeric'
-    }).split('/').join('-');
+    var formatoFecha4 = new Date(finalMes4);
 
     console.log(formatoFecha4);
     let fecha4: moment.Moment = moment(formatoFecha4);
@@ -249,11 +228,7 @@ export class GraficasPage implements OnInit {
     inicioMes3.setDate(1);
     inicioMes3.setMonth(date.getMonth() - 4);
 
-    var formatoFecha5 = new Date(inicioMes3).toLocaleDateString('en-GB', {
-      month: 'numeric',
-      day: 'numeric',
-      year: 'numeric'
-    }).split('/').join('-');
+    var formatoFecha5 = new Date(inicioMes3);
 
     console.log(formatoFecha5);
     let fecha5: moment.Moment = moment(formatoFecha5);
@@ -263,11 +238,7 @@ export class GraficasPage implements OnInit {
     finalMes3.setMonth(date.getMonth() - 4);
     finalMes3.setDate(31);
 
-    var formatoFecha6 = new Date(finalMes3).toLocaleDateString('en-GB', {
-      month: 'numeric',
-      day: 'numeric',
-      year: 'numeric'
-    }).split('/').join('-');
+    var formatoFecha6 = new Date(finalMes3);
 
     console.log(formatoFecha6);
     let fecha6: moment.Moment = moment(formatoFecha6);
@@ -277,11 +248,7 @@ export class GraficasPage implements OnInit {
     inicioMes2.setDate(1);
     inicioMes2.setMonth(date.getMonth() - 3);
 
-    var formatoFecha7 = new Date(inicioMes2).toLocaleDateString('en-GB', {
-      month: 'numeric',
-      day: 'numeric',
-      year: 'numeric'
-    }).split('/').join('-');
+    var formatoFecha7 = new Date(inicioMes2);
 
     console.log(formatoFecha7);
     let fecha7: moment.Moment = moment(formatoFecha7);
@@ -291,11 +258,7 @@ export class GraficasPage implements OnInit {
     finalMes2.setMonth(date.getMonth() - 3);
     finalMes2.setDate(30);
 
-    var formatoFecha8 = new Date(finalMes2).toLocaleDateString('en-GB', {
-      month: 'numeric',
-      day: 'numeric',
-      year: 'numeric'
-    }).split('/').join('-');
+    var formatoFecha8 = new Date(finalMes2);
 
     console.log(formatoFecha8);
     let fecha8: moment.Moment = moment(formatoFecha8);
@@ -305,11 +268,7 @@ export class GraficasPage implements OnInit {
     inicioMes1.setDate(1);
     inicioMes1.setMonth(date.getMonth() - 2);
 
-    var formatoFecha9 = new Date(inicioMes1).toLocaleDateString('en-GB', {
-      month: 'numeric',
-      day: 'numeric',
-      year: 'numeric'
-    }).split('/').join('-');
+    var formatoFecha9 = new Date(inicioMes1);
 
     console.log(formatoFecha9);
     let fecha9: moment.Moment = moment(formatoFecha9);
@@ -320,11 +279,7 @@ export class GraficasPage implements OnInit {
     finalMes1.setDate(31);
     //console.log(finalMes1.toLocaleDateString());
 
-    var formatoFecha10 = new Date(finalMes1).toLocaleDateString('en-GB', {
-      month: 'numeric',
-      day: 'numeric',
-      year: 'numeric'
-    }).split('/').join('-');
+    var formatoFecha10 = new Date(finalMes1);
 
     console.log(formatoFecha10);
     let fecha10: moment.Moment = moment(formatoFecha10);
@@ -332,11 +287,7 @@ export class GraficasPage implements OnInit {
     inicioMes6.setDate(1);
     inicioMes6.setMonth(date.getMonth() - 1);
 
-    var formatoFecha11 = new Date(inicioMes6).toLocaleDateString('en-GB', {
-      month: 'numeric',
-      day: 'numeric',
-      year: 'numeric'
-    }).split('/').join('-');
+    var formatoFecha11 = new Date(inicioMes6);
   
     console.log(formatoFecha11);
     let fecha11: moment.Moment = moment(formatoFecha11);
@@ -346,14 +297,10 @@ export class GraficasPage implements OnInit {
     finalMes6.setMonth(date.getMonth() - 1);
     finalMes6.setDate(31);
 
-    var formatoFecha12 = new Date(finalMes6).toLocaleDateString('en-GB', {
-      month: 'numeric',
-      day: 'numeric',
-      year: 'numeric'
-    }).split('/').join('-');
-  
-    console.log(formatoFecha12);
+    var formatoFecha12 = new Date(finalMes6);
+    //console.log(finalMes6.toLocaleDateString());
     let fecha12: moment.Moment = moment(formatoFecha12);
+    console.log(formatoFecha12);
     this.mesSeis=0;
     this.mesCinco=0;
     this.mesCuatro=0;
@@ -382,41 +329,37 @@ export class GraficasPage implements OnInit {
     for(var i=0; i<this.datosPedidos?.length; i++){
       if (this.idArticulo==this.datosPedidos[i]?.order_lines[0]?.articles_line[0]?.article_id){
         this.cantidadesArticulo+=this.datosPedidos[i].order_lines[0].articles_line[0].num_articles;
-        var fechaPedido = new Date(this.datosPedidos[i]?.order_lines[0]?.issue_date).toLocaleDateString('en-GB', {
-          month: 'numeric',
-          day: 'numeric',
-          year: 'numeric'
-        }).split('/').join('-');
+        var fechaPedido = new Date(this.datosPedidos[i]?.order_lines[0]?.issue_date);
          console.log(fechaPedido);
          let fecha: moment.Moment = moment(fechaPedido);
 
-        if(moment(fechaPedido)>=moment(fecha1)&&(moment(fechaPedido)<=moment(fecha2))){
+        if(moment(fecha)>=moment(fecha1)&&(moment(fecha)<=moment(fecha2))){
           console.log('mesSeis');
           console.log(this.mesSeis);
           this.mesSeis+=this.datosPedidos[i].order_lines[0].articles_line[0].num_articles;
         }
-        if(moment(fechaPedido)>=moment(fecha3)&&(moment(fechaPedido)<=moment(fecha4))){
+        if(moment(fecha)>=moment(fecha3)&&(moment(fecha)<=moment(fecha4))){
           console.log('mesCinco');
           console.log(this.mesCinco);
           this.mesCinco+=this.datosPedidos[i].order_lines[0].articles_line[0].num_articles;
         }
-        if(moment(fechaPedido)>=moment(fecha5)&&(moment(fechaPedido)<=moment(fecha6))){
+        if(moment(fecha)>=moment(fecha5)&&(moment(fecha)<=moment(fecha6))){
           console.log('mesCuatro');
           console.log(this.mesCuatro);
           this.mesCuatro+=this.datosPedidos[i].order_lines[0].articles_line[0].num_articles;
         }
-        if(moment(fechaPedido)>=moment(fecha7)&&(moment(fechaPedido)<=moment(fecha8))){
+        if(moment(fecha)>=moment(fecha7)&&(moment(fecha)<=moment(fecha8))){
           console.log('mesTres');
           console.log(this.mesTres);
           this.mesTres+=this.datosPedidos[i].order_lines[0].articles_line[0].num_articles;
         }
-        if(moment(fechaPedido)>=moment(fecha9)&&(moment(fechaPedido)<=moment(fecha10))){
+        if(moment(fecha)>=moment(fecha9)&&(moment(fecha)<=moment(fecha10))){
           console.log('mesDos');
           console.log(this.mesDos);
           this.mesDos+=this.datosPedidos[i].order_lines[0].articles_line[0].num_articles;
         }
         console.log(moment(fecha));
-        if(moment(fechaPedido)>=moment(fecha11)&&(moment(fechaPedido)<=moment(fecha12))){
+        if(moment(fecha)>=moment(fecha11)&&(moment(fecha)<=moment(fecha12))){
           console.log('mesUno');
           console.log(this.mesUno);
           this.mesUno+=this.datosPedidos[i].order_lines[0].articles_line[0].num_articles;
